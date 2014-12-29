4b9df201eee1b4b201c42c5de3c3a839d44a2c80
Francisco Quiñones Enciso <joseq@apesol.org.pe>
2014-12-28 20:13:51 -0500

    disable django_extension, not used

--- gtravel/gtravel/settings.py
+++ gtravel/gtravel/settings.py
@@ -128,7 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
+    #'django_extensions',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
